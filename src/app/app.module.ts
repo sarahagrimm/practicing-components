5b76b930273cba69302fec5073be301d6e8538c7
Sarah Grimm <sarahagrimm@gmail.com>
2018-06-05 16:54:32 -0700

    added buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { WarningMessageComponent } from './warning-message/warning-message.component';
+import { SuccessMessageComponent } from './success-message/success-message.component';
+import { DangerMessageComponent } from './danger-message/danger-message.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningMessageComponent,
+    SuccessMessageComponent,
+    DangerMessageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/success-message/success-message.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'success-message',
+  templateUrl: './success-message.component.html',
+  styleUrls: ['./success-message.component.css']
+})
+export class SuccessMessageComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+}
